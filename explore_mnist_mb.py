c1355583c55cf6e4c361a2d3e1c3292689c908ef
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2021-07-29 18:49:21 +0100

    first commit witha  working version that was originally coded by @neworderofjamie and tuned by @tnowotny

---
+++ explore_mnist_mb.py
@@ -0,0 +1,33 @@
+import mnist_mb
+import time
+from mnist_mb_para import para
+
+
+para["NUM_STIM_TRAIN"]= 100000
+para["NUM_STIM_TEST"]= 10000
+#para["RECORD_PN_SPIKES"]= True
+#para["RECORD_KC_SPIKES"]= True
+#para["RECORD_GGN_SPIKES"]= True
+para["rho"]= 0.01
+#para["plot"]= True
+for NKC in [20000 ]:
+    for nKC in [200]: #[ 20, 50, 100, 200, 500, 1000 ]: 
+        for wMax in [1]: #[ 5, 10, 20 ]:
+            for eta in [1]: #[ 0.00005, 0.0001, 0.0002]:
+                para["TRAIN"]= True
+                para["RECORD_MBON_SPIKES"]= False
+                para["NUM_KC"] = NKC
+                para["num_KC"]= nKC
+                para["wMax"]= 0.023316192520205694 #wMax*(1.0/(0.44324564+1.07*nKC))
+                para["eta"]= 2.03718224620342e-05 #eta*(1.0/(0.44324564+1.07*nKC))/0.04578126
+                today= time.strftime("%Y%m%d")
+                para["basename"]= today+"-NKC{}-nKC{}-wMax{}-eta{}-rep{}".format(para["NUM_KC"],para["num_KC"],para["wMax"],para["eta"],2)
+                para["STIM_MOD"]= 50000
+                para["SHUFFLE"]= True
+                g= mnist_mb.run_mb(para)
+                para["TRAIN"]= False
+                para["RECORD_MBON_SPIKES"]= True
+                para["STIM_MOD"]= 60000
+                para["SHUFFLE"]= False
+                para["STIM_SHIFT_TEST"]= 0
+                mnist_mb.run_mb(para,g)
